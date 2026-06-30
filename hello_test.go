3667897dba34b11cba829aede598828b8491a5b0
Xunni1e <xuansebastian2@gmail.com>
2026-06-29 23:19:49 -0500

    test and fnc hello refactor

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Juan")
-	want := "Hello, Juan"
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Juan")
+		want := "Hello, Juan"
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
